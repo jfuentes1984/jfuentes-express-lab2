422131f7e015e993249b44540773f3b8c6c35844
jfuentes1984 <6179215@cegep-heritage.qc.ca>
2022-03-09 16:11:38 -0500

    Let us commit our changes for lab 2!!

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'JIF Express Services' });
+});
+
+router.get('/subpathhandler', function (req, res, next) {
+  res.render('index', { title: 'I am NOT a page - I am a subpath handler!' });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  res.send('respond with a jif resource');
+});
+
+// takes this one first before below (user2)
+router.get('/:name', function (req, res, next) {
+  res.render('users', { 'name': req.params.name });
+});
+
+// takes first url above which is http://localhost:8080/users and you can add what's in after / to go into page
+// to use below url must be: http://localhost:8080/users/users2  
+router.get('/users2', function (req, res, next) {
+  res.send('respond with users2');
 });
 
 module.exports = router;
